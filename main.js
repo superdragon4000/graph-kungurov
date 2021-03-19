aae210788d2285a78ab8e42e0fda9409a28c61f2
Ilgizar Ismakaev <ilgizar3332@gmail.com>
2021-03-19 20:41:21 +0400

    added table constructor

--- main.js
+++ main.js
@@ -1,44 +1,11 @@
-const INPUT_TYPE = "input";
-const TEXT_TYPE = "value";
-
-function createTable() {
-
+function main() {
+	let table = document.getElementById("inputButton");
+	table.addEventListener("click", () => {
+		return document.getElementById("form").append(createTable());
+	}, false);
 }
-
-function createTableRow() {
-
-}
-
-function createTableElem() {
-	
-}
-
-function createInput() {
-	let input = document.createElement("input");
-	input.type = "number";
-	input.value = 0;
-	return input;
-}
-
-/**
- * Функция создания содержимого типа <span> для ячейки таблицы
- * 
- * @param {String} value - значение текстового поля
- */
-function createElement(value) {
-	let result = document.createElement("span");
-	result.value = value;
-	result.innerHTML = value;
-	return result;
-}
-
-function User(name) {
-	this.name = name;
-	this.isAdmin = false;
-}
-
-let user = new User("vasya");
 
 window.onload = function() {
 	document.getElementById("defaultOpen").click();
+	main();
 }
---
+++ table.js
@@ -0,0 +1,43 @@
+const INPUT_TYPE = "input";
+const TEXT_TYPE = "value";
+
+function createTable() {
+	let table = document.createElement("table");
+	let rows = document.getElementById("rows").value;
+	let columns = document.getElementById("columns").value;
+	for (let i = 0; i <= rows - 1; i++) {
+		table.append(createTableRow(columns));
+	}
+  table.className = "table";
+	return table;
+}
+
+function createTableRow(columns) {
+	let tableRow = document.createElement("tr");
+	for (let i = 0; i <= columns - 1; i++) {
+		tableRow.append(createTableElem())
+	}
+	return tableRow;
+}
+
+function createTableElem() {
+	let tableElem = document.createElement("td");
+  tableElem.className = "table_td";
+	tableElem.append(createInput());
+	return tableElem;
+}
+
+function createInput() {
+	let input = document.createElement("input");
+  input.className = "table_input";
+	input.type = "number";
+	input.value = 0;
+	return input;
+}
+
+function createElement(value) {
+	let result = document.createElement("span");
+	result.value = value;
+	result.innerHTML = value;
+	return result;
+}
